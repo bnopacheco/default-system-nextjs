f82ecb87b173b7544baf01dde1649ac5cb3f81c6
Julio Silva <julio@adsim.com.br>
2019-12-05 15:13:52 -0300

    Ajuste pwa

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,18 +17,16 @@ const MyApp = ({ Component, pageProps, store }) => {
             jssStyles.parentNode.removeChild(jssStyles);
         }
 
-        React.useEffect(() => {
-            if ('serviceWorker' in navigator) {
-              navigator.serviceWorker
-                .register('/service-worker.js')
-                .then((registration) => {
-                  console.log('service worker registration successful: ', registration);
-                })
-                .catch((err) => {
-                  console.warn('service worker registration failed', err.message);
-                });
-            }
-        }, []);
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker
+            .register('/service-worker.js')
+            .then((registration) => {
+                console.log('service worker registration successful: ', registration);
+            })
+            .catch((err) => {
+                console.warn('service worker registration failed', err.message);
+            });
+        }
 
     }, []);
 
