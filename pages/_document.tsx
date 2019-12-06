6b5a8e63fb36a2d0155337a2c367ca0b6d3b9f03
Julio Silva <julio@adsim.com.br>
2019-12-06 14:37:55 -0300

    Ajuste variáveis PWA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,7 +20,7 @@ class DocumentDefault extends Document {
             href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
           />
           <link rel='icon' type='image/x-icon' href='/static/favicon.ico' />
-          <link rel='icon' type='image/x-icon' sizes='192x192'  href='/static/icons/icon.png' />
+          <link rel='icon' type='image/x-icon' sizes='192x192'  href='/static/icons/icon-192x192.png' />
           <link rel='apple-touch-icon' href='/static/icons/apple-touch-icon' />
           <link rel='icon' type='image/x-icon' href='/static/icons/icon-512x512.png' />
           <meta name='msapplication-square310x310logo' content='/static/icons/icon_largetile.png'></meta>
