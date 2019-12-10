2d581a366d0d47b7de988542b83feb14e22c7afa
Julio Silva <julio@adsim.com.br>
2019-12-10 10:24:38 -0300

    Ajuste load files

--- next.config.js
+++ next.config.js
@@ -3,17 +3,32 @@ const webpack = require('webpack');
 const withOffline = require('next-offline')
 const withFonts = require('next-fonts');
 const withCSS = require('@zeit/next-css')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const PreloadWebpackPlugin = require('preload-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const PreloadWebpackPlugin = require('preload-webpack-plugin');
 
 const nextConfig = {
     webpack: (config, { isServer }) => {
         config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
-        config.plugins.push(new HtmlWebpackPlugin());
-        config.plugins.push(new PreloadWebpackPlugin({
-            rel: 'preload',
-            include: 'allAssets'
-        }));
+        // config.plugins.push(new HtmlWebpackPlugin());
+        // config.plugins.push(new PreloadWebpackPlugin({
+        //     rel: 'preload',
+        //     include: 'allAssets'
+        // }));
+        config.module.rules.push({
+            test: /\.(ico|gif|png|jpg|jpeg|svg|webp|woff|woff2)$/,
+            use: [
+              {
+                loader: 'emit-file-loader',
+                options: {
+                  name: '_next/static/chunks/fonts/[name].[ext]',
+                },
+              },
+              {
+                loader: 'file-loader',
+                options: {},
+              },
+            ],
+          });
 
         return config;
     },
--- utils/utils.tsx
+++ utils/utils.tsx
@@ -3,5 +3,5 @@ export default class Utils {
     public static firstUppercase(value: string) {
         return value[0].toUpperCase() + value.slice(1);
     }
-    
+
 }
